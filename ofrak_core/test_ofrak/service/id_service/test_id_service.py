9b1930ecb4c02ff8844c146a50e9c8a295cdb192
nicolaas <nicolaas@redballoonsecurity.com>
2025-09-16 17:46:50 -0400

    Remove dependence on pytest-lazy-fixtures

--- ofrak_core/test_ofrak/service/id_service/test_id_service.py
+++ ofrak_core/test_ofrak/service/id_service/test_id_service.py
@@ -5,49 +5,42 @@ from ofrak.service.id_service_sequential import SequentialIDService
 from ofrak.service.id_service_uuid import UUIDService
 
 
-@pytest.fixture()
+@pytest.fixture
 def sequential_id_service():
     return SequentialIDService()
 
 
-@pytest.fixture()
+@pytest.fixture
 def uuid_service():
     return UUIDService()
 
 
-@pytest.fixture(
-    params=[
-        pytest.lazy_fixture("sequential_id_service"),
-        pytest.lazy_fixture("uuid_service"),
-    ]
-)
-def id_service(request) -> IDServiceInterface:
-    return request.param
-
-
-def test_get_id(id_service):
-    id1 = id_service.generate_id()
-    id2 = id_service.generate_id()
+@pytest.mark.parametrize("id_service", ["sequential_id_service", "uuid_service"])
+def test_get_id(id_service, request):
+    id1 = request.getfixturevalue(id_service).generate_id()
+    id2 = request.getfixturevalue(id_service).generate_id()
 
     assert id1 != id2
 
 
-def test_generate_id_from_base(id_service):
-    base_id = id_service.generate_id()
+@pytest.mark.parametrize("id_service", ["sequential_id_service", "uuid_service"])
+def test_generate_id_from_base(id_service, request):
+    ids = request.getfixturevalue(id_service)
+    base_id = ids.generate_id()
     key1 = "alpha"
     key2 = "beta"
 
-    id1 = id_service.generate_id_from_base(base_id, key1)
-    id2 = id_service.generate_id_from_base(base_id, key2)
+    id1 = ids.generate_id_from_base(base_id, key1)
+    id2 = ids.generate_id_from_base(base_id, key2)
 
     assert id1 != id2
 
-    _id1 = id_service.generate_id_from_base(base_id, key1)
+    _id1 = ids.generate_id_from_base(base_id, key1)
     assert _id1 == id1
 
-    _id2 = id_service.generate_id_from_base(base_id, key2)
+    _id2 = ids.generate_id_from_base(base_id, key2)
     assert _id2 == id2
 
-    id3 = id_service.generate_id_from_base(id1, key2)
+    id3 = ids.generate_id_from_base(id1, key2)
     assert id3 != id1
     assert id3 != id2
